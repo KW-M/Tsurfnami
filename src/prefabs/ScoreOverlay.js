5ead757717e423ee1a7f7ae4d154caa43fcffbce
Kyle Worcester-Moore <25kylworc@gmail.com>
2021-04-26 22:51:49 -0700

    better doom bar Text

--- src/prefabs/ScoreOverlay.js
+++ src/prefabs/ScoreOverlay.js
@@ -11,12 +11,13 @@ export default class ScoreOverlay extends Phaser.GameObjects.GameObject {
         this.doomLevel = 1;
 
         // create an object to populate the text configuration members
-        let textConfig = {
+        this.textConfig = {
             fontFamily: "Courier",
             fontSize: "20px",
             backgroundColor: "#00b0ff85",
             color: "#FFFFFF",
-            align: "left",
+            align: "center",
+            fixedWidth: 0,
             padding: { top: 5, bottom: 5, left: 5, right: 5 },
         };
 
@@ -25,7 +26,7 @@ export default class ScoreOverlay extends Phaser.GameObjects.GameObject {
             0, // x-coord
             0, // y-coord
             this.formatTime(this.clock), // text to display
-            textConfig // text style config object
+            this.textConfig // text style config object
         );
 
         // add the event to increment the clock;
@@ -38,12 +39,12 @@ export default class ScoreOverlay extends Phaser.GameObjects.GameObject {
             scope: this,
             loop: true
         });
-        this.doomBar = this.scene.add.rectangle(60, 0, 1, 64, 'ff0000')
+        this.doomBar = this.scene.add.rectangle(62, 4, 1, 56, 0xff0000)
         this.doomText = this.scene.add.text(
             62,
             0,
             "DOOM: 0/100",
-            textConfig
+            this.textConfig
         )
 
         this.resize(this.scene.gameSize)
@@ -51,7 +52,9 @@ export default class ScoreOverlay extends Phaser.GameObjects.GameObject {
 
     resize(gameSize) {
         let fullWidth = gameSize.width - 62;
-        this.doomText.displayWidth = fullWidth
+        this.doomText.setStyle(Object.assign({}, this.textConfig, {
+            fixedWidth: fullWidth
+        }))
         this.doomBar.width = fullWidth * (this.doomLevel / 100)
     }
 
